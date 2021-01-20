ba11cb72a65a9997d8130e25eef013cd91e4ca4c
degortes <danilodegortes@gmal.com>
2021-01-20 17:35:03 +0100

    index is working as expected

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app');
+
+@section('content')
+    <header>
+        <h1>Boolando2</h1>
+    </header>
+    <section>
+        <div class="card-wrapper">
+
+        </div>
+    </section>
+@endsection
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <header>
+        <h2>index</h2>
+        <h1>Boolando2</h1>
+    </header>
+    <section>
+        <div class="card-wrapper">
+
+        </div>
+    </section>
+@endsection
